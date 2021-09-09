f25322789545ff832b246fd708027bbff087b15e
Norbert <norbert.zrupko@yahoo.com>
2021-09-09 11:56:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import tesmodul as tm # alias für das importierte Tesmodul vergeben
+
+print("ich bin die Main")
+
+
+x = 5
+y = 10
+
+gesamtheit = tm.summe(x, y) + tm.summe(x, y)
+print(gesamtheit)
+
+print(tm.NAME)
+
+NAME = "AUTO"
